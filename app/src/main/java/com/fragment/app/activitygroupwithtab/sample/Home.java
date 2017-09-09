ed0260c94dc830fb4d7a2d03c5b825f5f5c8a425
sunhongbo <sunhongbo@edaijia-inc.cn>
2017-09-09 15:40:31 +0800

    add toolbar

--- app/src/main/java/com/fragment/app/activitygroupwithtab/sample/Home.java
+++ app/src/main/java/com/fragment/app/activitygroupwithtab/sample/Home.java
@@ -1,8 +1,10 @@
 package com.fragment.app.activitygroupwithtab.sample;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.hongbo.lib.activitygrouplib.Activity;
+import com.hongbo.lib.activitygrouplib.ToolBar;
 
 /**
  * Created by sunhongbo on 17/8/22.
@@ -13,8 +15,12 @@ public class Home extends Activity {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
+        setToolBarEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_blank);
+        final ToolBar toolBar = getToolBar();
+        toolBar.setTitle("测试标题");
+        toolBar.setLeftText("左肩/可图");
         mTvDesc = (TextView) findViewById(R.id.tx_desc);
         mTvDesc.setText("this is 微信 tab");
     }
--- app/src/main/java/com/fragment/app/activitygroupwithtab/sample/Note.java
+++ app/src/main/java/com/fragment/app/activitygroupwithtab/sample/Note.java
@@ -1,9 +1,10 @@
 package com.fragment.app.activitygroupwithtab.sample;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.hongbo.lib.activitygrouplib.Activity;
+import com.hongbo.lib.activitygrouplib.ToolBar;
 
 /**
  * Created by sunhongbo on 17/8/22.
@@ -12,12 +13,14 @@ public class Note extends Activity {
 
     private TextView mTvDesc;
 
-    private int mType;
-
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
+        setToolBarEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_blank);
+        final ToolBar toolBar = getToolBar();
+        toolBar.setTitle("右肩膀");
+        toolBar.setCustomView(R.layout.tool_bar_refresh_icon);
         mTvDesc = (TextView) findViewById(R.id.tx_desc);
         mTvDesc.setText("this is 通讯录 tab");
     }
